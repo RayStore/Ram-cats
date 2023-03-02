bc1b543e472c7900f6eb157e6d098234f4520fca
RayStore <dragomir.newman@yandex.ru>
2023-03-02 13:19:41 +0300

    added project cats

---
+++ src/cats.js
@@ -0,0 +1,72 @@
+export const cats = [
+    {
+        id: 1,
+        name: "Барcи",
+        favorite: true,
+        rate: 9,
+        age: 2,
+        description: "Невская маскарадная",
+        img_link: 
+        "https://avatars.dzeninfra.ru/get-zen_doc/2977209/pub_5f9bb6e9c0a24c27870e1a1d_5f9bb8919e5bc33f0ddec24b/scale_1200"
+      },
+      {
+        id: 2,
+        name: "Васька",
+        favorite: false,
+        rate: 4,
+        age: 3,
+        description: "Абиссинская",
+        img_link: 
+        "https://www.purina.ru/sites/default/files/2021-10/abisinskaya-1.jpg"
+      },
+        {
+          id: 3,
+          name: "Вита",
+          favorite: true,
+          rate: 5,
+          age: 3,
+          description: "Американская короткошерстная",
+          img_link: 
+          "https://www.purina.ru/sites/default/files/2021-10/amer-korotkoserst-2.jpg"
+        },
+        {
+          id: 4,
+          name: "Вилли",
+          favorite: true,
+          rate: 6,
+          age: 2,
+          description: "Бирманская",
+          img_link: 
+          "https://www.purina.ru/sites/default/files/2021-10/birman-1.jpg"
+        },
+        {
+          id: 5,
+          name: "Брабус",
+          favorite: false,
+          rate: 8,
+          age: 3,
+          description: "Мейн-кун",
+          img_link: 
+          "https://www.purina.ru/sites/default/files/2021-10/mein-kun-2.jpg"
+        },
+        {
+          id: 6,
+          name: "Салли",
+          favorite: false,
+          rate: 5,
+          age: 4,
+          description: "Норвежская лесная",
+          img_link: 
+          "https://www.purina.ru/sites/default/files/2021-10/norvegskaya-2.jpg"
+        },
+        {
+          id: 7,
+          name: "Барри",
+          favorite: true,
+          rate: 10,
+          age: 2,
+          description: "Нибелунг",
+          img_link: 
+          "https://www.purina.ru/sites/default/files/2021-10/nibelung-2.jpg"
+        },
+      ];
