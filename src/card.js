47466db44f96b2f44f68cbe9c65f2ab29d96f622
RayStore <dragomir.newman@yandex.ru>
2023-03-03 19:05:19 +0300

    added modal-window

--- src/card.js
+++ src/card.js
@@ -2,17 +2,14 @@ export class Card {
     #data;
     #selectorTemplate;
     #element;
-    #handleClickCatImage;
     #handleCatTitle;
     #getTemplate(){
-        const template = document.querySelector(this.#selectorTemplate).content.querySelector('.card');
-        return template
+        return document.querySelector(this.#selectorTemplate).content.querySelector('.card');
     }
 
-    constructor(data, selectorTemplate, handleClickCatImage, handleCatTitle) {
+    constructor(data, selectorTemplate, handleCatTitle) {
         this.#data = data;
         this.#selectorTemplate = selectorTemplate;
-        this.#handleClickCatImage = handleClickCatImage;
         this.#handleCatTitle = handleCatTitle;
     }
 
@@ -21,20 +18,14 @@ export class Card {
         this.cardTitleElement = this.#element.querySelector('.card__name');
         this.cardImageElement = this.#element.querySelector('.card__image');
         this.cardLikeElement = this.#element.querySelector('.card__like');
-
-        this.cardTitleElement.textContent = this.#data.name
-        this.cardImageElement.src = this.#data.image
-
+        
         if(!this.#data.favorite) {
             this.cardLikeElement.remove()
         }
 
-        this.cardImageElement.addEventListener('click', () => {
-            this.#handleClickCatImage(this.#data.image);
-        })
-
-            this.setEventListener();
-        //Наполнять карточку
+        this.cardTitleElement.textContent = this.#data.name;
+        this.cardImageElement.src = this.#data.image;
+        this.setEventListener();
         return this.#element;
     }
 
